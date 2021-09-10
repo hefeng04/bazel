aae56b7d50f35f5dd74ea51a2c09f2e27d19c1c5
Feng He <feng.he@weride.ai>
2021-09-10 11:00:15 -0700

    python complicated example

--- python/py_deps.py
+++ python/py_deps.py
@@ -1,7 +1,6 @@
 import proto.api.api_pb2 as api_pb
 import proto.log.event_pb2 as event_pb
-import python.py_deps as py_deps
 
+def my_print():
+  print("This is py_deps")
 
-print("cool")
-py_deps.my_print()
